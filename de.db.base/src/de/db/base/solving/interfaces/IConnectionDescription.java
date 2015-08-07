8d700cc0c6200951b05509632d84f1227e6f32db
Basti <Basti@Basti-PC>
2015-08-07 18:52:09 +0200

    123

--- de.db.base/src/de/db/base/solving/interfaces/IConnectionDescription.java
+++ de.db.base/src/de/db/base/solving/interfaces/IConnectionDescription.java
@@ -2,4 +2,10 @@ package de.db.base.solving.interfaces;
 
 public interface IConnectionDescription {
 
+	public boolean equalTables();
+
+	public boolean directNeighbours();
+
+	public boolean isNeighbourTable();
+
 }
